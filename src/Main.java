f7f29d7430147f0a02da73d8369cbcf69cc8e3bf
zubin.huang <zubin.huang@mythware.com>
2023-04-13 09:38:09 +0800

    add processing code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
-public class Main {
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        PApplet.main("Main");
+    }
+
+    public void settings() {
+        size(800, 600);
+    }
+
+    public void setup() {
+        background(0);
+    }
+
+    public void draw() {
+        // Your Processing code here
     }
 }
